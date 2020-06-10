36ec26439c4d5d049047ddc2aee013d2e6a965fa
Kevin Dumont <kevin.dumont@seloger.com>
2020-06-10 23:08:35 +0200

    Fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,14 +45,11 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#F2FAFF`,
+        theme_color: `#FF5F2E`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon/logo.png`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
